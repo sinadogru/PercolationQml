21166b16f953e976c67fb376cdb7228d56c2586e
Cavit Sina Dogru <sinadooru@gmail.com>
2016-07-06 08:47:06 +0300

    Rounding mean and stddev to visualize.

--- percolationstats.cpp
+++ percolationstats.cpp
@@ -1,5 +1,6 @@
 #include "percolationstats.h"
-#include <QDebug>
+
+#include <cmath>
 
 PercolationStats::PercolationStats(QObject *parent)
     : QObject{parent},
@@ -39,14 +40,12 @@ void PercolationStats::calculateStats()
     const qreal sum = std::accumulate(m_stats.begin(), m_stats.end(), 0.);
 
     const qreal mean = sum / m_stats.size();
-    m_mean = mean;
+    m_mean = round(mean * 100) / 100;
     emit meanChanged();
 
     QVector<qreal> diff(m_stats.size());
     std::transform(m_stats.begin(), m_stats.end(), diff.begin(), [mean](double e) { return e - mean; });
     qreal sqSum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.);
-    m_stddev = std::sqrt(sqSum / m_stats.size());
+    m_stddev = round(std::sqrt(sqSum / m_stats.size()) * 100) / 100;
     emit stddevChanged();
-
-    qDebug() << "mean:" << mean << "sqSum:" << sqSum << "stddev:" << m_stddev;
 }
